9e5d0899f41ee8fdc8d259caa9d47ac9dbd84fc2
Saugat2002 <rockingsaugat777@gmail.com>
2023-12-05 22:06:22 +0545

    task2 and 4

---
+++ Task_04_Calculator/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="h-2/3 rounded-lg w-2/5 flex justify-center items-center bg-red-300">
+          <textarea name="screen" value={0} id="" cols="30" rows="10"></textarea>
+          Hello World
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
